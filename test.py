2561df69b0b337331a3ccdc72ee7e280fcd8182e
Natdanai Intraraksa <z0010220@pttgcgroup.com>
2023-09-13 15:56:53 +0700

    play video function

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import cv2
 import numpy as np
 
+cap = cv2.VideoCapture("data/register.mp4")
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    cv2.imshow("Video",frame)
+    if cv2.waitKey(1) == ord("q"):
+        break
+cap.release()
+cv2.destroyAllWindows()
+
 print(cv2.__version__)
